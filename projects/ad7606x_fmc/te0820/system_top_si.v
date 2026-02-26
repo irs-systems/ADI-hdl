ef7f1f3871acfd089a3d3b84faad2d25b78b389f
Matthias Diefenthaler <matthias.diefenthaler@irs.systems>
2026-02-26 09:30:26 +0100

    Port project from ZED board to TE0820

--- projects/ad7606x_fmc/te0820/system_top_si.v
+++ projects/ad7606x_fmc/te0820/system_top_si.v
@@ -36,33 +36,8 @@
 `timescale 1ns/100ps
 
 module system_top #(
-  parameter NUM_OF_SDI = 2
+  parameter NUM_OF_SDI = 1
 ) (
-  inout  [14:0] ddr_addr,
-  inout  [ 2:0] ddr_ba,
-  inout         ddr_cas_n,
-  inout         ddr_ck_n,
-  inout         ddr_ck_p,
-  inout         ddr_cke,
-  inout         ddr_cs_n,
-  inout  [ 3:0] ddr_dm,
-  inout  [31:0] ddr_dq,
-  inout  [ 3:0] ddr_dqs_n,
-  inout  [ 3:0] ddr_dqs_p,
-  inout         ddr_odt,
-  inout         ddr_ras_n,
-  inout         ddr_reset_n,
-  inout         ddr_we_n,
-
-  inout         fixed_io_ddr_vrn,
-  inout         fixed_io_ddr_vrp,
-  inout  [53:0] fixed_io_mio,
-  inout         fixed_io_ps_clk,
-  inout         fixed_io_ps_porb,
-  inout         fixed_io_ps_srstb,
-
-  inout  [31:0] gpio_bd,
-
   output        ad7606_spi_cs,
   output        ad7606_spi_sclk,
   input  [NUM_OF_SDI-1:0] ad7606_spi_sdi,
@@ -80,57 +55,28 @@ module system_top #(
 
   // internal signals
 
-  wire [63:0] gpio_i;
-  wire [63:0] gpio_o;
-  wire [63:0] gpio_t;
+  wire [31:0] gpio_i;
+  wire [31:0] gpio_o;
+  wire [31:0] gpio_t;
 
-  assign gpio_i[63:40] = gpio_o[63:40];
+  assign gpio_i[31:8] = gpio_o[31:8];
 
     // instantiations
 
   ad_iobuf #(
     .DATA_WIDTH(8)
   ) i_iobuf_adc_cntrl (
-    .dio_t (gpio_t[39:32]),
-    .dio_i (gpio_o[39:32]),
-    .dio_o (gpio_i[39:32]),
-    .dio_p ({adc_serpar,      // 39
-             adc_first_data,  // 38
-             adc_reset,       // 37
-             adc_stby,        // 36
-             adc_range,       // 35
-             adc_os}));       // 34:32
-
-  ad_iobuf #(
-    .DATA_WIDTH(32)
-  ) i_iobuf_gpio (
-    .dio_t(gpio_t[31:0]),
-    .dio_i(gpio_o[31:0]),
-    .dio_o(gpio_i[31:0]),
-    .dio_p(gpio_bd));
+    .dio_t (gpio_t[7:0]),
+    .dio_i (gpio_o[7:0]),
+    .dio_o (gpio_i[7:0]),
+    .dio_p ({adc_serpar,      // 7
+             adc_first_data,  // 6
+             adc_reset,       // 5
+             adc_stby,        // 4
+             adc_range,       // 3
+             adc_os}));       // 2:0    
 
   system_wrapper i_system_wrapper (
-    .ddr_addr (ddr_addr),
-    .ddr_ba (ddr_ba),
-    .ddr_cas_n (ddr_cas_n),
-    .ddr_ck_n (ddr_ck_n),
-    .ddr_ck_p (ddr_ck_p),
-    .ddr_cke (ddr_cke),
-    .ddr_cs_n (ddr_cs_n),
-    .ddr_dm (ddr_dm),
-    .ddr_dq (ddr_dq),
-    .ddr_dqs_n (ddr_dqs_n),
-    .ddr_dqs_p (ddr_dqs_p),
-    .ddr_odt (ddr_odt),
-    .ddr_ras_n (ddr_ras_n),
-    .ddr_reset_n (ddr_reset_n),
-    .ddr_we_n (ddr_we_n),
-    .fixed_io_ddr_vrn (fixed_io_ddr_vrn),
-    .fixed_io_ddr_vrp (fixed_io_ddr_vrp),
-    .fixed_io_mio (fixed_io_mio),
-    .fixed_io_ps_clk (fixed_io_ps_clk),
-    .fixed_io_ps_porb (fixed_io_ps_porb),
-    .fixed_io_ps_srstb (fixed_io_ps_srstb),
     .gpio_i (gpio_i),
     .gpio_o (gpio_o),
     .gpio_t (gpio_t),
